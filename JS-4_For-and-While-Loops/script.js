55b9504424cb6f7a2b5554fe5aff257c3ddb78c5
AyenMejorada <mejoradaayen1102@gmail.com>
2025-07-11 04:01:46 +0800

    update assignment 5

--- JS-4_For-and-While-Loops/script.js
+++ JS-4_For-and-While-Loops/script.js
@@ -5,9 +5,22 @@ for (let i = 1; i < 11; i++) {
 
 console.log(""); // for space
 console.log("While Loop Output");
-let hobbies = ["Traveling", "Reading", "Coding", "Drawing", "Playing ML", "Playing the piano", "Cleaning", "Jogging", "Watching Movie", "Sprinting", "Cycling", "Studying", "Exploring"]; // exceeding to more than 10 to check if the loop will really go up to 10 hobbies
-let i = 0;
-while (i < 10) { // has "(i+1) to not output the 0 (we started at 0)"
+let hobbies = [
+    "Traveling",
+    "Reading", 
+    "Coding", 
+    "Drawing", 
+    "Playing ML", 
+    "Playing the piano", 
+    "Cleaning", 
+    "Jogging", 
+    "Watching Movie", 
+    "Sprinting", 
+    "Cycling", 
+    "Studying", 
+    "Exploring"];
+let index = 0;
+while (index < hobbies.length) { // has "(i+1) to not output the 0 (we started at 0)"
     console.log("Hobby " + (i+1) + ": " + hobbies[i]);
-    i++
+    index++;
 }
---
+++ JS-5_Arrays-and-Loops/script.js
@@ -0,0 +1,6 @@
+console.log("My Top 5 Favorite Movies:");
+
+let favoriteMovies = ["Interstellar", "Inside Out", "The Darkest Minds", "Don't Look Up", "To All the Boys I've Loved Before"];
+for(let i = 0; i < 5; i++) {
+    console.log((i + 1) + ". " + favoriteMovies[i]);
+}
